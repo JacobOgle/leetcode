4bf2a7cf824acf9eb05f64d31dd9ee62e75dcb88
Jacob Ogle <123908271+JacobOgle@users.noreply.github.com>
2024-02-19 12:22:58 -0500

    Create 1_two_sum.py

---
+++ neetcode150/1_two_sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        out = []
+        # TC: O(n^2) -- double for loop iter n times each
+        # sliding window
+        for i in range(len(nums)):
+            # pointer increments
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i, j
+            
