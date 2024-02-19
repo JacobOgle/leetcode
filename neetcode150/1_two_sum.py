a0c3517d1ec914a5a7a846e05eca156046b76a65
Jacob Ogle <123908271+JacobOgle@users.noreply.github.com>
2024-02-19 12:29:02 -0500

    Update 1_two_sum.py

--- neetcode150/1_two_sum.py
+++ neetcode150/1_two_sum.py
@@ -1,4 +1,4 @@
-class Solution:
+class SolutionBruteForce:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # TC: O(n^2) -- double for loop iter n times each
         # sliding window
@@ -8,3 +8,17 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     return i, j
             
+class SolutionBetter:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # TC: O(n) - two for loops doing n work each time. unnested
+        map = {}
+
+        n = len(nums)
+
+        for i in range(n):
+            map[nums[i]] = i
+        
+        for i in range(n):
+            compl = target - nums[i]
+            if compl in map and map[compl] != i:
+                return i, map[compl]
